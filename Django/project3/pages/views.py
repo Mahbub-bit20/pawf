44dae4df1feeef82e31ac9c8474e25ce96877d4b
Haidar Arif Mahbub <arifmahbubhaidar@gmail.com>
2026-05-28 17:37:06 +0700

    Add gitignore, requirements, and initial project setup

---
+++ Django/project3/pages/views.py
@@ -0,0 +1,14 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def home_page_view(request):
+	return HttpResponse("Homepage")
+
+
+def about_page_view(request):
+	context = {
+		"name": "Haidar Arif Mahbub",
+		"age": 21,
+	}
+	return render(request, "pages/about.html", context)
