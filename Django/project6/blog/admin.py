bc02e0e5013d070dd11cd1079e1adf92d83fbef9
Haidar Arif Mahbub <arifmahbubhaidar@gmail.com>
2026-07-09 18:28:28 +0700

    Add project blog website with model, admin, views, urls, templates, static files, detail page, get_absolute_url, and tests

--- Django/project6/blog/admin.py
+++ Django/project6/blog/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from .models import Post
 
 class PostAdmin(admin.ModelAdmin):
-    list_display = ("title", "author", "created_at") # Tambahkan created_at di sini
+    list_display = ("title", "author", "created_at") 
 
 admin.site.register(Post, PostAdmin)
